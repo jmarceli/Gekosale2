d05516e4a267ecee90972ae67c6b763c2f558e55
Jan Grzegorowski <grzegorowski@gmail.com>
2015-03-03 20:52:56 +0100

    add payment method info text when no delivery method is selected

--- sql/update_20150303.sql
+++ sql/update_20150303.sql
@@ -20,4 +20,15 @@ INSERT INTO translationdata (translation, translationid, languageid)
   FROM translation
   WHERE name = 'ERR_DELIVERY_SELECT_DESC'
     ON DUPLICATE KEY UPDATE translation = 'Prosimy o wybór sposobu dostawy w celu złożenia zamówienia.';
+-- translation for TXT_COOKIE_SECOND
+INSERT INTO translation (name)
+  SELECT 'TXT_SELECT_DELIVERY_BEFORE_PAYMENT'
+  FROM translation
+  WHERE NOT EXISTS (SELECT * FROM translation WHERE name = 'TXT_SELECT_DELIVERY_BEFORE_PAYMENT')
+  LIMIT 1;
+INSERT INTO translationdata (translation, translationid, languageid)
+  SELECT 'Sposoby płatności pojawią się po wybraniu sposobu dostawy.', idtranslation, 1
+  FROM translation
+  WHERE name = 'TXT_SELECT_DELIVERY_BEFORE_PAYMENT'
+    ON DUPLICATE KEY UPDATE translation = 'Sposoby płatności pojawią się po wybraniu sposobu dostawy.';
 
